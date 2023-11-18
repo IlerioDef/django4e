dd563ca329e423e71028af7db1584b440ea54142
IlerioDef <47086473+IlerioDef@users.noreply.github.com>
2023-11-17 20:56:41 -0500

    step 13 (Cookies and Sessions) - done

--- django4e/urls.py
+++ django4e/urls.py
@@ -3,6 +3,7 @@ import os
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
+from django.views.generic import TemplateView
 from django.views.static import serve
 
 from django4e import settings
@@ -12,6 +13,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SITE_ROOT = os.path.join(BASE_DIR, 'site')
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home/main.html')),
+    path('hello/', include('hello.urls')),
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
     re_path(r'^site/(?P<path>.*)$', serve,
