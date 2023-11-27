896fffb499a10b56c39b8174cfd4965e9bf3c3de
IlerioDef <47086473+IlerioDef@users.noreply.github.com>
2023-11-27 00:47:49 -0500

    step 22 (Classified Ads #1) - done

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from django.views.generic import TemplateView
-
 from . import views
+from django.contrib.auth import views as auth_views
 
-app_name = "home"
-
-urlpatterns = []
+urlpatterns = [
+    path('', views.HomeView.as_view()),
+]
