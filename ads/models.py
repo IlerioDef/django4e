caa20b91e9cde03065ff21bbacc7cda08c472dc9
IlerioDef <47086473+IlerioDef@users.noreply.github.com>
2023-12-04 20:04:52 -0500

    step 31 (Classified Ads#4) - done

--- ads/models.py
+++ ads/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
 from django.conf import settings
+from taggit.managers import TaggableManager
 
 
 class Ad(models.Model):
@@ -20,6 +21,7 @@ class Ad(models.Model):
     content_type = models.CharField(max_length=256, null=True, help_text='The MIMEType of the file')
     favorites = models.ManyToManyField(settings.AUTH_USER_MODEL,
                                        through='Fav', related_name='favorite_ads')
+    tags = TaggableManager(blank=True)
 
     # Shows up in the admin list
     def __str__(self):
